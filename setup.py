58fa9a7198b96c9181aea8c5437963775b8ae695
codingaround <codingaround90s@gmail.com>
2021-12-20 22:11:58 +0330

    9th

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="CAImport",
-    version="1.0.5",
+    version="1.0.6",
     description="Coding Around simple tools for python",
     long_description=README,
     long_description_content_type="text/markdown",
